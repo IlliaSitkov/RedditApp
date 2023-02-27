ba9649e68a5189a467b4a24d866fab84f04691da
illia_sitkov <illiasitkov@gmail.com>
2023-02-27 12:51:19 +0200

    change some icons to SF Symbols, change author label format

--- RedditApp/ViewController.swift
+++ RedditApp/ViewController.swift
@@ -12,33 +12,42 @@ class ViewController: UIViewController, PostManagerDelegate {
     
     var service = PostManager()
     
+    let bookmarkSet = UIImage(systemName: "bookmark.circle.fill") ?? UIImage(systemName: "bookmark.fill")
+    let bookmarkUnset = UIImage(systemName: "bookmark.circle") ?? UIImage(systemName: "bookmark")
+    
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var domainLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var commentsBtn: UIButton!
     @IBOutlet weak var likesLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var bookmarkBtn: UIButton!
-    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var upvotesImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        commentsBtn.setImage(UIImage(systemName: "plus.message"), for: .normal)
+        upvotesImg.image = UIImage(systemName: "arrow.up.heart")        
+        
         service.delegate = self
         service.getPostsWithParams(subreddit: "ios", limit: 1)
     }
     
     func postsFetched(posts: PostResponseData) {
+        guard posts.data.children.count > 0 else {return}
         let post = posts.data.children[0].data;
         DispatchQueue.main.async {
             self.titleLabel.text = post.title
-            self.timeLabel.text = self.convert(time: Date().timeIntervalSince1970 - Double(post.created))
-            self.domainLabel.text = post.domain
-            self.authorLabel.text = post.username
+            let time = self.convert(time: Date().timeIntervalSince1970 - Double(post.created))
+            let domain = post.domain
+            self.authorLabel.text = "\(post.username) • \(time) • \(domain)"
             self.likesLabel.text = String(post.ups - post.downs)
             self.commentsBtn.setTitle(String(post.numComments), for: .normal)
-            self.imageView.sd_setImage(with: URL(string: post.preview.images[0].source.url.replacingOccurrences(of: "amp;", with: "")), placeholderImage: nil)
-            self.bookmarkBtn.setImage(UIImage(systemName: post.saved ? "bookmark.fill" : "bookmark"), for: .normal)
+            if post.preview.images.count > 0 {
+                self.imageView.sd_setImage(with: URL(string: post.preview.images[0].source.url.replacingOccurrences(of: "amp;", with: "")), placeholderImage: nil)
+            }
+            self.bookmarkBtn.setImage(post.saved ? self.bookmarkSet : self.bookmarkUnset, for: .normal)
         }
     }
     
